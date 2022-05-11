7ac5aa531207f7d83a5ae8e1de52dd243afc8eae
brandums <brrandum151@gmail.com>
2022-05-11 17:05:39 -0400

    Homework

---
+++ OOP/Generics/Model/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Model
+{
+    class Subject : IMyModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Root()
+        {
+            return "Resources/Subjects.txt";
+        }
+
+        public IMyModel ToModel(string str)
+        {
+            var array = str.Split('-');
+            return new Subject
+            {
+                Id = array[0],
+                Name = array[1],
+            };
+        }
+    }
+}
