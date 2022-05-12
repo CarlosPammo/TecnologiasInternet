3ef881c266499763fbab36e8d5188bf9e09f3a17
Carlos Pammo <Carlos.Pammo@cirruslogistics.com>
2022-05-11 20:28:26 -0400

    Complete the Repository Pattern

--- EFESample/EFESample/Program.cs
+++ EFESample/EFESample/Program.cs
@@ -11,16 +11,38 @@ namespace EFESample
 {
 	class Program
 	{
+
+
+
 		static void Main(string[] args)
 		{
-			var repository = new Repository<Student>(new BaseContext());
+			var repository = new Repository<Career>(new BaseContext());
+
+			#region ==== Insert ====
 			//repository.Insert(new Student
 			//{
 			//	Name = "Elias",
 			//	Lastname = "Chavarria"
 			//});
+			#endregion
 
-			Console.WriteLine(repository.Select(s => true).First().Name);
+			#region ==== Update ====
+			//var student = repository
+			//	.Select(s => s.Name.Equals("Elias"))
+			//	.First();
+
+			//student.Name = "Elias Augusto";
+			//repository.Update(student);
+			#endregion
+
+			#region ==== Delete ====
+			//repository.Delete(s => s.Lastname.Equals("Chavarria"));
+			#endregion
+
+			foreach (var item in repository.Select(s => true))
+			{
+				Console.WriteLine(item);
+			}
 
 			Console.ReadKey();
 		}
--- EFESample/EntityAccess/Repository.cs
+++ EFESample/EntityAccess/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using EntityAccess.Context;
 using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -27,6 +28,30 @@ namespace EntityAccess
 			var entry = Context.Set<T>().Add(entity);
 			Context.SaveChanges();
 			return entry;
+		}
+
+		public int Update(T entity)
+		{
+			var entry = Context.Entry(entity);
+			Context.Set<T>().Attach(entity);
+			entry.State = EntityState.Modified;
+			return Context.SaveChanges();
+		}
+
+		public int Delete(Expression<Func<T, bool>> lambda)
+		{
+			var matches = Select(lambda);
+			if (matches == null)
+			{
+				return 0;
+			}
+
+			foreach (T match in matches)
+			{
+				Context.Set<T>().Remove(match);
+			}
+			Context.SaveChanges();
+			return matches.Count();
 		}
 
 		public IQueryable<T> Select(Expression<Func<T, bool>> lambda)
